1b515f0f1364b2825f933478443af986e223e737
413kouzo <badra2ali.dembele@gmail.com>
2023-12-30 04:53:39 +0000

    style Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,13 +3,36 @@ import DefaultPicture from '../../assets/profile.png'
 import styled from 'styled-components'
 import colors from '../../utils/style/color'
 
+const CardLabel = styled.span`
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
+`
+
+const CardTitle = styled.span`
+  color: black;
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+`
+
+const CardImage = styled.img`
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
+`
+
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   padding: 15px;
   background-color: ${colors.backgroundLight};
   border-radius: 30px;
-  width350px;
+  width: 300px;
+  height: 300px;
   transition: 200ms;
   &:hover {
     cursor: pointer;
@@ -17,27 +40,16 @@ const CardWrapper = styled.div`
   }
 `
 
-const CardLabel = styled.span`
-  color: #5843e4;
-  font-size: 22px;
-  font-weight: bold;
-`
-
-const CardImage = styled.img`
-  height: 80px;
-  width: 80px;
-  border-radius: 50%;
-`
-
 function Card({ label, title, picture }) {
   return (
     <CardWrapper>
       <CardLabel>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
-      <span>{title}</span>
+      <CardTitle>{title}</CardTitle>
     </CardWrapper>
   )
 }
+
 Card.propTypes = {
   label: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ const StyledLink = styled(Link)`
   color: #8186a0;
   text-decoration: none;
   font-size: 18px;
+  text-align: center;
   ${(props) =>
     props.$isFullLink &&
     `

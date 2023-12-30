3c8b690ebec98014d762563fcfae9eb6559d1896
413kouzo <badra2ali.dembele@gmail.com>
2023-12-30 02:38:53 +0000

    ajout de Card et exercice sur propTypes

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,35 @@
+import Card from '../../components/Card'
+import DefaultPicture from '../../assets/profile.png'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Developpeur Frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Developpeuse Fullstack',
+    picture: DefaultPicture,
+  },
+]
 function Freelances() {
   return (
     <div>
       <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
     </div>
   )
 }
